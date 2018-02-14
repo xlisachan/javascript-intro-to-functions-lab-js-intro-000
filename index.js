45ceefca4383b5d5b97d06ef4ced09cc2370e97b
Lisa Chan <xlisachan@gmail.com>
2018-02-14 23:22:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase());
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  }
+}
